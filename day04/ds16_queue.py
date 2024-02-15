e2782d94b21069a7ebd74f688d35013fc82015ff
guswlrla <rlaguswl_01@naver.com>
2024-02-15 11:26:32 +0900

    큐 개선버전으로 변경

--- day04/ds16_queue.py
+++ day04/ds16_queue.py
@@ -1,12 +1,20 @@
 # date : 2024-02-15
 # desc : 큐 일반구현
 
-# Queue 풀 함수
+# Queue 풀 확인함수
 def isQueueFull():
-    global SIZE, rear
-    if rear == (SIZE - 1):
+    global SIZE, rear, front, rear
+    if rear != (SIZE - 1): # 큐가 아직 빈상태
+        return False
+    elif rear == (SIZE-1) and front == -1: # 큐가 꽉 찬 상태
         return True
-    else:
+    else: # 큐가 앞쪽이 비어있는 상태, rear가 끝까지 간 상태
+        while front != -1: # 완전히 앞으로 당긴다. front가 -1이 될때까지
+            for i in range(front+1, SIZE):
+                queue[i-1] = queue[i] # front에다가 front+1의 값을 할당
+                queue[i] = None
+            front -= 1
+            rear -= 1
         return False
     
 # Queue 엠티 확인함수
@@ -50,13 +58,13 @@ def peek():
 
 
 # 전역변수
-SIZE = input(int('큐 크기 입력(정수) > ')) # 상수(constant)
+SIZE = int(input('큐 크기 입력(정수) > ')) # 상수(constant)
 queue = [None for _ in range(SIZE)]
 front = rear = -1
 
 if __name__ == '__main__': # 메인시작
     while True:
-        select = input('삽입(e), 추출(d), 확인(p), 종료(x)')
+        select = input('삽입(e), 추출(d), 확인(p), 종료(x) ')
 
         if select.lower() == 'e':
             data = input('입력 데이터 > ')
